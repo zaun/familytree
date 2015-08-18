0db3051d36043d6d573cd76e8d37277490884f04
Justin Zaun <justin.zaun@gmail.com>
2015-08-17 17:48:56 -1000

    Forcing a push

--- server/api/image.js
+++ server/api/image.js
@@ -85,7 +85,7 @@ var processResizes = function () {
       setTimeout(processResizes, 500);
     });
   } else {
-    setTimeout(processResizes, 1000);
+    setTimeout(processResizes, 500);
   }
 };
 
